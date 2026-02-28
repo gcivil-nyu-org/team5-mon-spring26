eadc80c30c1a091e597022147651529f07136568
Ananya Singh <ananyasingh@ananyas-air.mynetworksettings.com>
2026-02-28 14:17:21 -0500

    adding path

--- treestagram_project/treestagram/urls.py
+++ treestagram_project/treestagram/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -27,4 +27,8 @@ urlpatterns = [
 
                   # Django template views (server-rendered fallback)
                   path('', include('accounts.urls')),
+                  re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
+
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    

ca11f8731878f08e75868b93a7b44a7d23370ba7
Ramsey <0monsuno82@gmail.com>
2026-03-19 17:56:55 -0400

    maybe marker fix pulling png directly from url

--- treestagram_project/treestagram-svelte/src/routes/Map.svelte
+++ treestagram_project/treestagram-svelte/src/routes/Map.svelte
@@ -8,6 +8,16 @@
 
   import "leaflet/dist/leaflet.css";
 
+  // FIX: Leaflet marker icons (MUST be here)
+  delete L.Icon.Default.prototype._getIconUrl;
+
+  L.Icon.Default.mergeOptions({
+    iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+    iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+    shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+  });
+    //  FIX: Leaflet marker icons (MUST be here)
+
   let map;
   let trees = [];
 

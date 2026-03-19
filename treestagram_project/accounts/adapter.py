77309d926f7c67de4ddd5b526faf396e36a94b92
Ramsey <0monsuno82@gmail.com>
2026-03-18 23:51:43 -0400

    reversed adapter.py

--- treestagram_project/accounts/adapter.py
+++ treestagram_project/accounts/adapter.py
@@ -1,37 +1,12 @@
-# import os
-# from allauth.account.adapter import DefaultAccountAdapter
-# class TreestagramAccountAdapter(DefaultAccountAdapter):
-#     def confirm_email(self, request, email_address):
-#         super().confirm_email(request, email_address)
-#         user = email_address.user
-#         if not user.is_active:
-#             user.is_active = True
-#             user.save(update_fields=['is_active'])
-#     def get_email_confirmation_redirect_url(self, request):
-#         base_url = os.environ.get('FRONTEND_URL', 'http://localhost:5173')
-#         return f'{base_url}/login?confirmed=true'   
-
-
 import os
 from allauth.account.adapter import DefaultAccountAdapter
-from allauth.account.models import EmailAddress
-
 class TreestagramAccountAdapter(DefaultAccountAdapter):
-
-    # Keep your existing confirm_email logic
     def confirm_email(self, request, email_address):
         super().confirm_email(request, email_address)
         user = email_address.user
         if not user.is_active:
             user.is_active = True
             user.save(update_fields=['is_active'])
-
-    # Optional: keep redirect after clicking confirmation
     def get_email_confirmation_redirect_url(self, request):
         base_url = os.environ.get('FRONTEND_URL', 'http://localhost:5173')
-        return f'{base_url}/login?confirmed=true'
-
-    # NEW: override the email link itself to use FRONTEND_URL
-    def get_email_confirmation_url(self, request, emailconfirmation):
-        frontend_url = os.environ.get('FRONTEND_URL', 'http://localhost:5173')
-        return f"{frontend_url}/accounts/confirm-email/{emailconfirmation.key}"
+        return f'{base_url}/login?confirmed=true'   

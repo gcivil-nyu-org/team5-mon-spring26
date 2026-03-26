1bdc7885abc8f65757ad7b809a0871ed2f1b347d
Sumit <sumit8692@gmail.com>
2026-03-26 02:19:51 -0400

    Fix asgi.py import order and formatting

--- treestagram_project/treestagram/asgi.py
+++ treestagram_project/treestagram/asgi.py
@@ -1,18 +1,19 @@
 import os
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "treestagram.settings")
 
-# This must come before any Django/channels imports
-from django.core.asgi import get_asgi_application
+from django.core.asgi import get_asgi_application  # noqa: E402
+
 django_asgi_app = get_asgi_application()
 
-# Only import channels/routing AFTER Django is initialized
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import chat.routing
+from channels.auth import AuthMiddlewareStack  # noqa: E402
+from channels.routing import ProtocolTypeRouter, URLRouter  # noqa: E402
 
-application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-    "websocket": AuthMiddlewareStack(
-        URLRouter(chat.routing.websocket_urlpatterns)
-    ),
-})
+import chat.routing  # noqa: E402
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns)),
+    }
+)

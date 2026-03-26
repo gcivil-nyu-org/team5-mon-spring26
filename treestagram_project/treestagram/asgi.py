e193eff04cbc8a65e5c9e1cfaec4d71845d7b896
Sumit <sumit8692@gmail.com>
2026-03-26 02:09:24 -0400

    Fix nginx websocket conf and asgi import order

--- treestagram_project/treestagram/asgi.py
+++ treestagram_project/treestagram/asgi.py
@@ -1,25 +1,18 @@
-"""
-ASGI config for treestagram project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
-
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "treestagram.settings")
+
+# This must come before any Django/channels imports
+from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
+
+# Only import channels/routing AFTER Django is initialized
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat.routing
-from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "treestagram.settings")
-
-# Initialize Django ASGI application early
-django_asgi_app = get_asgi_application()
-application = ProtocolTypeRouter(
-    {
-        "http": django_asgi_app,
-        "websocket": AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns)),
-    }
-)
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(
+        URLRouter(chat.routing.websocket_urlpatterns)
+    ),
+})
